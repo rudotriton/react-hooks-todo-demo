6683e08a227cf50a769fd658d1770619d60c922f
Raigo Jerva <raigo.jerva@gmail.com>
2019-08-20 22:47:07 +0300

    save tasks to indexeDB

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,8 @@
 import React, {
-  createContext, useContext, useReducer, useState,
+  createContext, useContext, useEffect, useReducer, useState,
 } from 'react';
 import styled from 'styled-components';
+import localforage from 'localforage';
 import filterReducer from './filterReducer';
 import todoReducer from './todoReducer';
 
@@ -30,8 +31,19 @@ const TodoItem = ({
     <StyledTodoItem style={{ textDecoration: isCompleted ? 'line-through' : '' }}>
       {task}
       <div>
-        <button type="button" onClick={changeComplete}>Complete</button>
-        <button type="button" onClick={removeTodo}>X</button>
+        <Button
+          type="button"
+          onClick={changeComplete}
+          style={{ marginRight: '4px' }}
+        >
+          {isCompleted ? 'Undo' : 'Complete'}
+        </Button>
+        <Button
+          type="button"
+          onClick={removeTodo}
+        >
+          X
+        </Button>
       </div>
     </StyledTodoItem>
   );
@@ -54,7 +66,7 @@ const TodoForm = () => {
       <Input
         type="text"
         value={value}
-        onChange={e => setValue(e.target.value)}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="add todo"
       />
     </form>
@@ -80,39 +92,26 @@ const Filter = () => {
   };
   return (
     <HeadingWrapper>
-      <button type="button" onClick={handleShowAll}>
+      <Button type="button" onClick={handleShowAll}>
         Show All
-      </button>
-      <button type="button" onClick={handleShowComplete}>
+      </Button>
+      <Button type="button" onClick={handleShowComplete}>
         Show Complete
-      </button>
-      <button type="button" onClick={handleShowIncomplete}>
+      </Button>
+      <Button type="button" onClick={handleShowIncomplete}>
         Show Incomplete
-      </button>
+      </Button>
     </HeadingWrapper>
   );
 };
 
 // This is the whole todo component that wraps the filters, list and input
 const Todo = () => {
-  const [todos, dispatchTodos] = useReducer(todoReducer, [
-    {
-      task: 'learn about hooks',
-      isCompleted: false,
-    },
-    {
-      task: 'design ui for app',
-      isCompleted: false,
-    },
-    {
-      task: 'write a song',
-      isCompleted: false,
-    },
-  ]);
+  const [todos, dispatchTodos] = useReducer(todoReducer, []);
   const [filter, dispatchFilters] = useReducer(filterReducer, 'ALL');
 
   // global dispatch function, this is like combineReducers in Redux
-  const dispatch = action => [dispatchTodos, dispatchFilters].forEach(fn => fn(action));
+  const dispatch = (action) => [dispatchTodos, dispatchFilters].forEach((fn) => fn(action));
 
   const filteredTodos = todos.filter((todo) => {
     if (filter === 'ALL') {
@@ -126,6 +125,37 @@ const Todo = () => {
     }
     return false;
   });
+
+  useEffect(() => {
+    localforage.getItem('todo-hooks')
+      .then((val) => {
+        if (typeof val === 'object' && val.length === 0) {
+          dispatch({
+            type: 'SET_TODOS',
+            todos: [
+              {
+                task: 'Add some new tasks',
+                isCompleted: false,
+              },
+            ],
+          });
+        } else if (val !== null) {
+          dispatch({
+            type: 'SET_TODOS',
+            todos: val,
+          });
+        }
+      })
+      .catch(() => {
+        dispatch({
+          type: 'SET_TODOS',
+          todos: [{
+            task: 'Could not fetch todo items',
+            isCompleted: false,
+          }],
+        });
+      });
+  }, []);
   return (
     // we provide our dispatch function to all components
     <DispatchContext.Provider value={dispatch}>
@@ -153,7 +183,7 @@ const Todo = () => {
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: #209cee;
+  background-color: #070231;
   color: white;
   display: flex;
   flex-direction: column;
@@ -161,7 +191,7 @@ const Wrapper = styled.div`
 `;
 
 const HeadingWrapper = styled.div`
-  padding: 5px;
+  padding: 5px 0;
   display: flex;
   justify-content: space-between;
 `;
@@ -171,7 +201,7 @@ const ListWrapper = styled.div`
   background: #e8e8e8;
   border-radius: 4px;
   padding: 5px;
-  min-width: 400px;
+  min-width: 500px;
   color: black;
 `;
 
@@ -179,7 +209,7 @@ const StyledTodoItem = styled.div`
   background: #fff;
   box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
   padding: 3px 10px;
-  font-size: 12px;
+  font-size: 16px;
   margin-bottom: 6px;
   border-radius: 3px;
   display: flex;
@@ -189,12 +219,27 @@ const StyledTodoItem = styled.div`
 
 const Input = styled.input`
   width: 100%;
+  font-size: 16px;
   box-sizing: border-box;
   border: 0;
   border-radius: 4px;
   box-shadow: none;
   padding: 1rem;
   height: 2.25em;
+`;
+
+const Button = styled.button`
+  font-size: 14px;
+  border-radius: 4px;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    background: #000;
+    color: #fff;
+  }
 `;
 
 export default Todo;
--- src/todoReducer.js
+++ src/todoReducer.js
@@ -1,26 +1,42 @@
+import localforage from 'localforage';
+
 const todoReducer = (state, action) => {
   switch (action.type) {
-    case 'DO_TODO':
-      return state.map((todo, index) => {
+    case 'DO_TODO': {
+      const newState = state.map((todo, index) => {
         if (index === action.index) {
           return { ...todo, isCompleted: true };
         }
         return todo;
       });
-    case 'UNDO_TODO':
-      return state.map((todo, index) => {
+      localforage.setItem('todo-hooks', newState);
+      return newState;
+    }
+    case 'UNDO_TODO': {
+      const newState = state.map((todo, index) => {
         if (index === action.index) {
           return { ...todo, isCompleted: false };
         }
         return todo;
       });
-    case 'ADD_TODO':
-      return state.concat({
+      localforage.setItem('todo-hooks', newState);
+      return newState;
+    }
+    case 'ADD_TODO': {
+      const newState = state.concat({
         task: action.task,
         isCompleted: false,
       });
-    case 'REMOVE_TODO':
-      return state.slice(0, action.index).concat(state.slice(action.index + 1));
+      localforage.setItem('todo-hooks', newState);
+      return newState;
+    }
+    case 'REMOVE_TODO': {
+      const newState = state.slice(0, action.index).concat(state.slice(action.index + 1));
+      localforage.setItem('todo-hooks', newState);
+      return newState;
+    }
+    case 'SET_TODOS':
+      return action.todos;
     default:
       return state;
   }
